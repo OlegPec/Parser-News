0993ab55e4abb860a9b597fcb164556a0535ed3e
OlegPec <oleg.petsura@yandex.ru>
2019-12-16 20:48:21 +0300

    fix kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -31,7 +31,7 @@ class Kernel extends ConsoleKernel
                   ->everyMinute();*/
         /* $schedule->command('runtask')
                   ->everyMinute();*/
-        $schedule->command(RssParserController::class)->everyMinute();
+        $schedule->command(RssParserController::class)->everyTenMinutes();
     }
 
     /**
