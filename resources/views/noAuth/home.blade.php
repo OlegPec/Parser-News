e3eaff3a0e9769d63f7fb197a1434bb861767b16
OlegPec <oleg.petsura@yandex.ru>
2020-03-09 14:56:59 +0300

    upd header

--- resources/views/noAuth/home.blade.php
+++ resources/views/noAuth/home.blade.php
@@ -9,10 +9,19 @@
         .height-img {
             height: 240px;
         }
+        .bg-black {
+            background: #000;
+        }
+        .card a {
+            color: black;
+        }
     </style>
 
+    <nav class="navbar navbar-dark bg-black justify-content-center">
+        <a class="navbar-brand" href="{{ route('home') }}">evolnews</a>
+    </nav>
     <div class="container">
-        <div class="d-flex justify-content-between"><h1>Новости</h1><span>@if(Auth::user()) Привет {{ Auth::user()->name }} <a href="{{ route('logout') }}" onclick="event.preventDefault()
+        <div class="d-flex justify-content-between"><span>@if(Auth::user()) Привет {{ Auth::user()->name }} <a href="{{ route('logout') }}" onclick="event.preventDefault()
                 document.getElementById('logout-form').submit();" class="">
                     {{ __('Выйти') }}</a>
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -33,10 +42,12 @@
                                 <img src="{{ asset('images/news/default_img.svg') }}" alt="Card image cap" class="img-fluid">
                             @endif
                         </a>
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $element->title }}</h5>
-                            <p class="card-text">{{ rtrim(mb_strimwidth($element->description, 0, 252))."..." }}</p>
-                        </div>
+                        <a href="{{ route('showNews', $element->id) }}">
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $element->title }}</h5>
+                                <p class="card-text">{{ rtrim(mb_strimwidth($element->description, 0, 252))."..." }}</p>
+                            </div>
+                        </a>
                         <div class="card-footer">
 {{--                            <small class="text-muted">Last updated 3 mins ago</small>--}}
                             <small class="text-muted">{{ $element->public_date }}</small>
